0051d81a9ce826f9f44c05a4e152cc94d6838e97
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-05-13 10:19:51 -0400

    lonelyinteger

---
+++ lonelyInt.js
@@ -0,0 +1,19 @@
+function lonelyinteger(a) {
+    while (a.length > 1) {
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 1; j < a.length; j++) {
+            console.log(`a[i] = ${a[i]} a[j] = ${a[j]}`)
+            if (a[j] === a[i]) {
+                a.splice(a.indexOf(a[j]), 1)
+                a.splice(a.indexOf(a[i]), 1)
+                i = 0
+                console.log(`current array: ${a}`)
+            }
+        } 
+        return console.log(a[i])
+    }
+}
+    return console.log(a[0])
+}
+
+lonelyinteger([7, 0, 34, 7, 34, 0, 18, 18, 47, 46, 33, 47, 33, 46, 8])
