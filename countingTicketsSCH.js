9c8dcb123a06f4bbc887ee644edf3ba5a4bc5f5d
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-05-26 19:28:10 -0400

    alternate solution for countingTickets

--- countingTicketsSCH.js
+++ countingTicketsSCH.js
@@ -76,19 +76,57 @@ function canParkClose(record) {
     return count === leaveCount ? 'CLOSE' : 'OPEN';
   }
 
-  function canParkClose(record) {
-    let stack = []
+  ///////////////////////////////////////////////////////////
+  function canParkClose1(record) {
+    let count = []
+    let leaveCount = []
 
-    for (let i=0; i<record.length; i++) {
-      const num = record[i]
-      // check if num is positive
-      if (num < 0) {
-        // if it is a negative number in the first position of the record arr, the record is invalid
-        if (stack.length < 0) {
-          return 'OPEN'
+    // helper function to sort arr's
+    function sort(arr) {
+      for (let i=1; i<arr.length; i++) {
+        let placeholder = 0
+        if (arr[i] < arr[i-1]) {
+           placeholder = arr[i-1]
+           arr[i-1] = arr[i]
+           arr[i] = placeholder
         }
-        // if the negative record is equal to the record at the top of the stack (last index of record arr)
-        if (Math.abs(num) === record.length - 1)
       }
     }
+
+    // loop through record
+    for (let i=0; i<record.length; i++) {
+      const num = record[i]
+
+      // if num is 0, remove it from record arr. Later we will compare the count + leaveCount arr's by length, so any 0's will break this logic.
+      if (num === 0) {
+        record.slice(i, 1)
+        i--
+      }
+
+      // check if number is negative
+      if (num < 0) {
+        // if it is a negative number in the first position of the record arr, the record is invalid
+        if (count.length === 0) {
+          return 'OPEN'
+        }
+        // if the negative record is equal to the record at the top of the stack (last index of record arr), remove the group that's on top of the stack, else push that neg value to the 
+        Math.abs(num) === record[record.length - 1] ? count.pop() : leaveCount.push(num)
+      } else {
+        // if num is positive, add it to count arr
+        count.push(num)
+      }
+    }
+
+    if (count.length === leaveCount.length) {
+      sort(count)
+      sort(leaveCount)
+      for (let i=0; i<count.length; i++) {
+        if (count[i] != leaveCount[i]) {
+          return 'OPEN'
+        }
+      }
+    return 'CLOSE'
   }
+}
+
+console.log(canParkClose([3, 4, 7, 16, -7, -16, 2, -2, -4, -3]))
