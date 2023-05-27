74613391c66ad97d69125888d692f0acbdf87ca6
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-05-27 15:39:14 -0400

    findTheBalance, adjusted so that the loop will return the answer without finishing the loop

--- findTheBalanceSCH.js
+++ findTheBalanceSCH.js
@@ -50,11 +50,11 @@
 // }
 
 function findBalanceIndex(arr) {
-    // initialize variable to keep track of sum of all arr elements
+    // init var to hold the sum of all arr elements
     let totalSum = arr.reduce((sum, num) => sum + num, 0);
-    // initialize variable to keep track of leftSum, default zero
+    // init var to 0 to hold sum of elements on left of current index
     let leftSum = 0;
-    // initialize variable to keep track of minimum difference between left + right sums. We use Infinity because it will be an undetermined positive integer that we will decrement as we loop through the arr.
+    // init var to keep track of min difference between left + right sums. We use Infinity because it will be an undetermined positive integer that we will decrement as we loop through the arr.
     let minDiff = Infinity;
     // -1 is the default case and will be returned if the arr is empty or has no valid answer.
     let balanceIndex = -1;
@@ -63,21 +63,22 @@ function findBalanceIndex(arr) {
     for (let i = 0; i < arr.length; i++) {
       // subtract the leftSum and the current arr element from totalSum to get the rightSum
       let rightSum = totalSum - leftSum - arr[i];
-      // the diff is the absolute difference between the two sums. 
+      // the diff is the absolute difference between the two sums at the particular index in the loop. 
       let diff = Math.abs(leftSum - rightSum);
 
-      // if the difference between the two sums is less than the minDiff (positive Infinity), then set minDiff to diff. If the diff == minDiff this logic will be skipped and the lowest balanceIndex will still get returned.
+      // if the difference between the sums is less than the minDiff, set minDiff to diff. If the diff == minDiff this logic will be skipped and the lowest balanceIndex will still get returned.
       if (diff < minDiff) {
         minDiff = diff;
         // set balanceIndex to that current index in the loop
         balanceIndex = i;
+      } else {
+        // when diff is no longer < minDiff, the ideal answer is found. return balanceIndex
+        return balanceIndex
       }
         // increment the leftSum by the value of current arr element before the loop restarts
+      console.log(`leftSum: ${leftSum}\nrightSum: ${rightSum}\ndiff: ${diff}\nminDiff: ${minDiff}`)
       leftSum += arr[i];
     }
-    
-    // when diff is no longer < minDiff, the ideal answer is found. 
-    return balanceIndex;
   }
 
-findBalance([2, 4, 5, 1, 7, 2, 1, 4])
+console.log(findBalanceIndex([4, 5, 86, 12, 87, 29, 91, 46, 4, 85,1, 90, 34, 75, 15, 87]))
